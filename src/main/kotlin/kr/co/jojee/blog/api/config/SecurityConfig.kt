2606cd7e81c55b5402675ed6052b951dd242366b
Jo-Jee <capjjo@gmail.com>
2022-12-25 17:40:05 +0900

    jwt 사용해서 stateless로세션 인증설정 변경

--- src/main/kotlin/kr/co/jojee/blog/api/config/SecurityConfig.kt
+++ src/main/kotlin/kr/co/jojee/blog/api/config/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
@@ -34,6 +35,8 @@ class SecurityConfig(
             .anyRequest().permitAll()
             .and()
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
         return http.build()
     }
