1e90048bd33e544b63670cbf7334f3f72647b030
Jo-Jee <capjjo@gmail.com>
2023-02-18 07:49:11 +0900

    test 정상화

--- src/test/kotlin/kr/co/jojee/blog/api/PostTest.kt
+++ src/test/kotlin/kr/co/jojee/blog/api/PostTest.kt
@@ -1,9 +1,11 @@
-package kr.co.jojee.blog
+package kr.co.jojee.blog.api
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PostTest {
 
     @Test
